3c91c7aa33c5128a7c07cd3de5273664289add39
carlynorama <carlynorama@gmail.com>
2023-03-25 18:29:14 -0700

    add package, add buttons

--- UnsafeExplorer/ContentView.swift
+++ UnsafeExplorer/ContentView.swift
@@ -6,16 +6,46 @@
 //
 
 import SwiftUI
+import UWCSampler
 
 struct ContentView: View {
+    let rand = RandomNumberSetProvider()
+    @State var myNum:Int = 0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world! (\(myNum))")
+            Button("New Number") {
+                updateNumber()
+            }
+            Button("Print random numbers") {
+                printRandos()
+            }
+            Button("Print random in range") {
+                printPlusRandos()
+            }
         }
         .padding()
+    }
+    
+    func updateNumber() {
+        myNum = rand.getRandomInt()
+    }
+    
+    func printRandos() {
+        let vals = rand.makeArrayOfRandomInt(count:10)
+        print(vals)
+    }
+    
+    func printPlusRandos() {
+        let vals = rand.randomValueInRange(min: 45, max: 234, count: 12)
+        print(vals)
+        
+        let vals2 = rand.addRandomTo([345, 773, 983, 8827, 1], upTo:50)
+        print(vals2)
     }
 }
 
